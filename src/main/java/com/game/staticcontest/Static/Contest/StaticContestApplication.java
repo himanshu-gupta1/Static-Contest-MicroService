b18f1af55c9480a6394f99fbd8de2426856d3c90
himanshu gupta <creative.himanshu1@gmail.com>
2019-01-29 00:29:57 +0530

    project initialized

---
+++ src/main/java/com/game/staticcontest/Static/Contest/StaticContestApplication.java
@@ -0,0 +1,14 @@
+package com.game.staticcontest.Static.Contest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticContestApplication.class, args);
+	}
+
+}
+
