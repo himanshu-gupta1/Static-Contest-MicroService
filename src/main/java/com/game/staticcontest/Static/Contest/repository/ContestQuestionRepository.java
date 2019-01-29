ed9d75a2f6538fc7f4a3946de2c399af8202e277
nitinmathew <nitin.mathew0211@gmail.com>
2019-01-29 18:30:03 +0530

    Contest Question Controller Done

--- src/main/java/com/game/staticcontest/Static/Contest/repository/ContestQuestionRepository.java
+++ src/main/java/com/game/staticcontest/Static/Contest/repository/ContestQuestionRepository.java
@@ -1,4 +1,10 @@
 package com.game.staticcontest.Static.Contest.repository;
 
-public interface ContestQuestionRepository {
+import com.game.staticcontest.Static.Contest.dto.QuestionDetailDTO;
+import com.game.staticcontest.Static.Contest.entity.ContestQuestion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContestQuestionRepository extends CrudRepository<ContestQuestion, String> {
+
+    ContestQuestion findByQuestionSequence(String questionSequence);
 }
--- src/main/java/com/game/staticcontest/Static/Contest/service/ContestQuestionService.java
+++ src/main/java/com/game/staticcontest/Static/Contest/service/ContestQuestionService.java
@@ -1,4 +1,10 @@
 package com.game.staticcontest.Static.Contest.service;
 
+import com.game.staticcontest.Static.Contest.dto.ResponseDTO;
+import com.game.staticcontest.Static.Contest.entity.ContestQuestion;
+
 public interface ContestQuestionService {
+
+    ResponseDTO<Void> addQuestion(ContestQuestion contestQuestion);
+
 }
