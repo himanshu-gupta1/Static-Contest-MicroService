0a7fe3adced6ed6a04a0813abbf4f3326b7c67df
himanshu gupta <creative.himanshu1@gmail.com>
2019-01-29 18:31:06 +0530

    done

--- src/main/java/com/game/staticcontest/Static/Contest/controller/ContestPlayAreaController.java
+++ src/main/java/com/game/staticcontest/Static/Contest/controller/ContestPlayAreaController.java
@@ -1,8 +1,10 @@
 package com.game.staticcontest.Static.Contest.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class ContestPlayAreaController {
 
 
--- src/main/java/com/game/staticcontest/Static/Contest/controller/ContestSubscriptionController.java
+++ src/main/java/com/game/staticcontest/Static/Contest/controller/ContestSubscriptionController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/contests/{contestId}")
+@CrossOrigin
 public class ContestSubscriptionController {
 
 
